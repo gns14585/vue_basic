a8f1f3db390778d83e56a2c2db2d580f1340139c
이정훈 <gns14585@naver.com>
2024-04-15 16:55:44 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div>
-    <ChildComponent
-      :sendProps1="title"
-      :sendProps2="createAt"
-      :sendProps3="obj"
-    />
-  </div>
+  <div>TEST</div>
 </template>
 
-<script setup lang="ts">
-import ChildComponent from "@/components/ChildComponent.vue";
-import { reactive, ref } from "vue";
-
-interface Obj {
-  id: number;
-  name: string;
-}
-
-const title = ref<string>("부모 컴포넌트에서 선언된 데이터 입니다.");
-const createAt = ref<number>(2024);
-const obj = reactive<Obj>({
-  id: 2024,
-  name: "John",
-});
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
